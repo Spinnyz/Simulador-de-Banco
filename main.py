289ab3f17db8e4391c0524ed03337d1c0e64e3ea
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-08-04 17:36:00 -0300

    up

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from datetime import date
+
+#Cliente
+class cliente:    
+    def endereco(self, endereco):
+        self.endereco = endereco
+        self.contaconta = []
+    
+    def adicionarconta(self,conta):
+        self.conta.append(conta)
+    
+    def 
+
+        
+        
+    
